bedcae6bc20b08951131feabcd6c9ac4265df17a
Kawase-Ryo <2801r.kawase@gmail.com>
2022-07-20 14:43:58 +0900

    Update calendars_controller.rb

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,7 +2,7 @@ class CalendarsController < ApplicationController
 
   # １週間のカレンダーと予定が表示されるページ
   def index
-    get_Week
+    getWeek
     @plan = Plan.new
   end
 
@@ -20,7 +20,7 @@ class CalendarsController < ApplicationController
 
   def get_Week
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
-   
+
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
     # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
@@ -34,6 +34,7 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
+<<<<<<< HEAD
 
       wday_num = Date.today.wday + x # wdayメソッドを用いて取得した数値
       if wday_num >= 7   #「wday_numが7以上の場合」という条件式
@@ -43,14 +44,20 @@ class CalendarsController < ApplicationController
       end
 
       days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wday => wdays[wday_num]}
+=======
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
+>>>>>>> parent of 60bf90c (issue6)
       @week_days.push(days)
     end
 
   end
 end
+<<<<<<< HEAD
 
 # [1] → [Date.today.wday + x]
 # wdays[1] → wdays[Date.today.wday + x]
 # wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']wdaysから曜日を取り出すには、wdays[添字]
 # 7.times do |x| xは7週するので、x=0 ~ x=6 まで代入される。
 # Date.today.wday → 今日の曜日に対して 0~6の数字で返す 例:今日が月曜日ならDate.today.wday
+=======
+>>>>>>> parent of 60bf90c (issue6)
